5e1ca3e94255893027ae95109291e3cc47de4147
Pavel Boldyrev <wyx2021@gmail.com>
2012-10-27 15:46:40 -0400

    - GPL licence - Java 1.7

--- src/main/java/net/galaxy/rfbee/CommandMode.java
+++ src/main/java/net/galaxy/rfbee/CommandMode.java
@@ -1,3 +1,21 @@
+/*
+ * Project: rfbee-java, file: CommandMode.java
+ * Copyright (C) 2012 Pavel Boldyrev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.galaxy.rfbee;
 
 /**
@@ -32,9 +50,9 @@ public interface CommandMode {
     }
 
     /**
-     * Sets address check mode. 0 - NONE by default.
+     * Sets address check mode.
      *
-     * @param addressCheck
+     * @param addressCheck address check mode, 0 - NONE by default
      */
     void setAddressCheck(AddressCheck addressCheck);
 
